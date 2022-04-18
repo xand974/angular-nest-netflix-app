cbbeb27a9bd5a5b5c50c0b749092e7a78c62b6c0
Alexandre <xand974@gmail.com>
2022-04-18 22:48:33 +0200

    remove unused middleware

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,13 +1,7 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TokenModule } from './token/token.module';
-import { AdminMiddleware } from './middlewares/admin.middleware';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { UserInfosModule } from './user-infos/user.infos.module';
@@ -28,14 +22,4 @@ import { RolesGuard } from './guards/role.guard';
   ],
   providers: [{ provide: APP_GUARD, useClass: RolesGuard }],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(AdminMiddleware)
-      .forRoutes(
-        { path: 'api/users', method: RequestMethod.ALL },
-        { path: 'api/user-infos', method: RequestMethod.ALL },
-        { path: 'api/films', method: RequestMethod.ALL },
-      );
-  }
-}
+export class AppModule {}
