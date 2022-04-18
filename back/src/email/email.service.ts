063276a24d161a85518c48ebcdb9330ea9eb3cce
Alexandre <xand974@gmail.com>
2022-04-18 22:45:09 +0200

    handle email verification

--- back/src/email/email.service.ts
+++ back/src/email/email.service.ts
@@ -2,14 +2,16 @@ import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TokenService } from 'src/token/token.service';
+import { registerVerificationTemplate } from './templates/email-verification';
 
 @Injectable()
 export class EmailService {
   constructor(
     private readonly tokenService: TokenService,
-    private configService: ConfigService,
-    private mailerService: MailerService,
+    private readonly configService: ConfigService,
+    private readonly mailerService: MailerService,
   ) {}
+
   public async sendEmailVerification({
     email,
     isVerified,
@@ -23,11 +25,12 @@ export class EmailService {
     )}?token=${token}`;
     const text = `Welcome to the application ! Click on the link to finish the registration`;
     const from = this.configService.get('FROM');
+    const emailTemplate = registerVerificationTemplate(email, url);
     await this.mailerService.sendMail({
       to: email,
       from,
+      html: emailTemplate,
       subject: text,
-      html: `<h1>Hello There , please click hier : ${url}</h1>`,
     });
   }
 }
