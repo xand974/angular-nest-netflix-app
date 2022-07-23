2d93219e1e1d7356200b2403f32a3d96672c293c
xand974 <xand974@gmail.com>
2022-07-23 15:48:16 +0200

    wip watch ui + print infos on pause

--- front/src/app/pages/watch/watch.component.ts
+++ front/src/app/pages/watch/watch.component.ts
@@ -1,4 +1,14 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  HostListener,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { WatchStore } from './watch.store';
 import { HomeService } from '../home/home.service';
@@ -12,22 +22,45 @@ import { Observable } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [WatchStore],
 })
-export class WatchComponent implements OnInit {
+export class WatchComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild('videoControl') videoControl: ElementRef<HTMLVideoElement>;
   movie$: Observable<MovieModel | null>;
   public id: string;
+  public isPause: boolean;
+  private mouseMoving: boolean;
+  private trackInterval: any;
+  private trackTimeout: any;
+
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private cStore: WatchStore,
-    private homeService: HomeService
+    private homeService: HomeService,
+    private cdr: ChangeDetectorRef
   ) {
     this.id = '';
     this.movie$ = this.cStore.movie$;
+    this.videoControl = {} as ElementRef<HTMLVideoElement>;
+    this.isPause = false;
+    this.mouseMoving = false;
+  }
+
+  ngAfterViewInit(): void {
+    this.subscribeToPlayPause();
   }
 
   ngOnInit(): void {
     this.initParam();
     this.initMovie();
+    // this.initInterval();
+  }
+
+  private initInterval() {
+    this.trackInterval = setInterval(() => {
+      if (this.isPause && !this.mouseMoving) {
+        this.isPause = true;
+      }
+    }, 1000);
   }
 
   initParam() {
@@ -48,5 +81,38 @@ export class WatchComponent implements OnInit {
       this.cStore.setLoading(false);
       this.cStore.setError(true);
     }
+  }
+
+  public subscribeToPlayPause(): void {
+    if (!this.videoControl.nativeElement) return;
+    this.videoControl.nativeElement.onpause = (event) => {
+      setTimeout(() => {
+        this.displayInfos(true);
+      }, 1000);
+      return;
+    };
+    this.videoControl.nativeElement.onplay = (event) => {
+      this.displayInfos(false);
+      return;
+    };
+  }
+
+  private displayInfos(value: boolean) {
+    this.isPause = value;
+    this.cdr.detectChanges();
+  }
+
+  //#region HostListener
+  @HostListener('mousemove')
+  public onGlobalMouseMove() {
+    this.mouseMoving = true;
+    if (this.isPause) {
+      this.isPause = false;
+    }
+  }
+  //#endregion
+
+  ngOnDestroy(): void {
+    clearInterval(this.trackInterval);
   }
 }
